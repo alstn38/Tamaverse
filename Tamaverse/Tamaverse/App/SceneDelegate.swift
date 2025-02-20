ee11c2f8d6ea0d972d610de7e1ceffb14a6d41e2
Minsoo Kang <alstn38@naver.com>
2025-02-20 19:10:59 +0900

    init: Tamaverse project 생성

---
+++ Tamaverse/Tamaverse/App/SceneDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  SceneDelegate.swift
+//  Tamaverse
+//
+//  Created by 강민수 on 2/20/25.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let scene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: scene)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) { }
+
+    func sceneDidBecomeActive(_ scene: UIScene) { }
+
+    func sceneWillResignActive(_ scene: UIScene) { }
+
+    func sceneWillEnterForeground(_ scene: UIScene) { }
+
+    func sceneDidEnterBackground(_ scene: UIScene) { }
+}
---
+++ Tamaverse/Tamaverse/Present/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Tamaverse
+//
+//  Created by 강민수 on 2/20/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .cyan
+    }
+}
