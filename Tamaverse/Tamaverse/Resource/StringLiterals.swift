46e51a6c2f75f3678fe59573cfe04fb5caa665f5
Minsoo Kang <alstn38@naver.com>
2025-02-20 20:14:28 +0900

    chore: StringLiterals SelectTama Title 추가

---
+++ Tamaverse/Tamaverse/Resource/StringLiterals.swift
@@ -0,0 +1,15 @@
+//
+//  StringLiterals.swift
+//  Tamaverse
+//
+//  Created by 강민수 on 2/20/25.
+//
+
+import Foundation
+
+enum StringLiterals {
+    
+    enum SelectTama {
+        static let title: String = "다마고치 선택하기"
+    }
+}
