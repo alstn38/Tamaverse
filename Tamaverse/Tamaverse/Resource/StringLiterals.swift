4df5fa99aa65841dd571163ca92404b74e7c05d8
Minsoo Kang <alstn38@naver.com>
2025-02-21 01:41:45 +0900

    chore: StringLiterals SelectTama Button Title 추가

--- Tamaverse/Tamaverse/Resource/StringLiterals.swift
+++ Tamaverse/Tamaverse/Resource/StringLiterals.swift
@@ -11,5 +11,8 @@ enum StringLiterals {
     
     enum SelectTama {
         static let title: String = "다마고치 선택하기"
+        static let cancelButtonTitle: String = "취소"
+        static let startButtonTitle: String = "시작하기"
+        static let changeButtonTitle: String = "변경하기"
     }
 }
