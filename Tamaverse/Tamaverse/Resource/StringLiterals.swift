eea76cea8d000ae548c887baa5965e31d45f5dfa
Minsoo Kang <alstn38@naver.com>
2025-02-21 17:21:39 +0900

    chore: StringLiterals SelectTama Navigation Title, Alret text 추가

--- Tamaverse/Tamaverse/Resource/StringLiterals.swift
+++ Tamaverse/Tamaverse/Resource/StringLiterals.swift
@@ -9,10 +9,17 @@ import Foundation
 
 enum StringLiterals {
     
+    enum Alert {
+        static let confirm: String = "확인"
+    }
+    
     enum SelectTama {
-        static let title: String = "다마고치 선택하기"
+        static let selectTitle: String = "다마고치 선택하기"
+        static let changeTitle: String = "다마고치 변경하기"
         static let cancelButtonTitle: String = "취소"
         static let startButtonTitle: String = "시작하기"
         static let changeButtonTitle: String = "변경하기"
+        static let changeAlertTitle: String = "변경 완료"
+        static let changeAlertMessage: String = "다마고치가 변경되었습니다."
     }
 }
