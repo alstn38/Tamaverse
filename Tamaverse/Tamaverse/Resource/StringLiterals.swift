a7497e35b42dad8e97391c838b7ff21d69a4a874
Minsoo Kang <alstn38@naver.com>
2025-02-21 20:01:42 +0900

    chore: StringLiterals Game Text 추가

--- Tamaverse/Tamaverse/Resource/StringLiterals.swift
+++ Tamaverse/Tamaverse/Resource/StringLiterals.swift
@@ -22,4 +22,12 @@ enum StringLiterals {
         static let changeAlertTitle: String = "변경 완료"
         static let changeAlertMessage: String = "다마고치가 변경되었습니다."
     }
+    
+    enum Game {
+        static let separatorText: String = "·"
+        static let foodTextPlaceholder: String = "밥주세용"
+        static let waterTextPlaceholder: String = "물주세용"
+        static let foodButtonTitle: String = "밥먹기"
+        static let waterButtonTitle: String = "물먹기"
+    }
 }
