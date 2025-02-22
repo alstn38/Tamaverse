0a90610d2a8247932b1876bfa8036c90544cf11b
Minsoo Kang <alstn38@naver.com>
2025-02-23 01:37:09 +0900

    feat: UIViewController+Extension delete present method 추가

--- Tamaverse/Tamaverse/Extension/UIViewController+.swift
+++ Tamaverse/Tamaverse/Extension/UIViewController+.swift
@@ -24,4 +24,28 @@ extension UIViewController {
         alertController.addAction(alertAction)
         present(alertController, animated: true)
     }
+    
+    func presentDeleteAlert(title: String, message: String, completion: @escaping () -> Void) {
+        let alertController = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle: .alert
+        )
+        
+        let cancelAlertAction = UIAlertAction(
+            title: StringLiterals.Alert.cancel,
+            style: .default
+        )
+        
+        let deleteAlertAction = UIAlertAction(
+            title: StringLiterals.Alert.delete,
+            style: .destructive
+        ) { _ in
+            completion()
+        }
+        
+        alertController.addAction(cancelAlertAction)
+        alertController.addAction(deleteAlertAction)
+        present(alertController, animated: true)
+    }
 }
