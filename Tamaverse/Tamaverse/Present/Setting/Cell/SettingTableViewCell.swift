77895f4303f7b559174c339f512f47db03af70d7
Minsoo Kang <alstn38@naver.com>
2025-02-23 23:41:18 +0900

    feat: SettingTableViewCell 구현

---
+++ Tamaverse/Tamaverse/Present/Setting/Cell/SettingTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  SettingTableViewCell.swift
+//  Tamaverse
+//
+//  Created by 강민수 on 2/23/25.
+//
+
+import UIKit
+
+final class SettingTableViewCell: UITableViewCell, ReusableViewProtocol {
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: .value1, reuseIdentifier: reuseIdentifier)
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
